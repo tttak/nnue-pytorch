cce5e1b3de59b8dbe67a9a3d494b86f65dc9683b
Hisayori Noda <nodchip@gmail.com>
2021-05-07 17:59:23 +0900

    CPUが使用できないバグを修正した

--- train.py
+++ train.py
@@ -90,7 +90,12 @@ def main():
   checkpoint_callback = pl.callbacks.ModelCheckpoint(save_last=True)
   trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback], logger=tb_logger)
 
-  main_device = trainer.root_device if trainer.root_gpu is None else 'cuda:' + str(trainer.root_gpu)
+  if not args.gpus:
+    main_device = 'cpu'
+  elif trainer.root_gpu:
+    main_device = 'cuda:' + str(trainer.root_gpu)
+  else:
+    main_device = trainer.root_device
 
   if args.py_data:
     print('Using python data loader')
